196605d74ff804449b9da4e1256a2f4169ce8ebe
Marquito94 <marco.fernandezrolon@gmail.com>
2023-11-01 15:39:22 -0300

    Added first AWS files

--- instance.tf
+++ instance.tf
@@ -1,3 +1,14 @@
+resource "aws_instance" "ec2" {
+  ami           = "ami-08cba41c585e4a2e2"
+  instance_type = "t3.micro"
+
+  security_groups = [aws_security_group.secgroup.name]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
 resource "aws_security_group" "secgroup" {
 	name = "terraform-tcp-security-group"
 	
@@ -6,16 +17,5 @@ resource "aws_security_group" "secgroup" {
 		to_port = 22
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "ec2" {
-  ami           = ami-08cba41c585e4a2e2
-  instance_type = "t3.micro"
-
-  security_groups = [aws_security_group.secgroup.name]
-
-  tags = {
-    Name = "HelloWorld"
   }
 }
