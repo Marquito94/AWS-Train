718c0e45cc162cc1490df2bfab03fc2e89349c93
Marquito94 <71236169+Marquito94@users.noreply.github.com>
2023-11-01 15:46:36 -0300

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,3 +1,14 @@
+resource "aws_security_group" "secgroup" {
+  name = "terraform-tcp-security-group"
+ 
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "ec2" {
   ami           = "ami-08cba41c585e4a2e2"
   instance_type = "t3.micro"
@@ -6,15 +17,5 @@ resource "aws_instance" "ec2" {
 
   tags = {
     Name = "HelloWorld"
-  }
-}
-
-resource "aws_security_group" "secgroup" {
-  name = "terraform-tcp-security-group"
-  ingress {
-    from_port = 2
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
