25d48ab48cfe4af654fb66f5ad3c4e6f07f3fe64
Marquito94 <marco.fernandezrolon@gmail.com>
2023-11-01 20:38:16 -0300

    Added files

--- WebApplicationV1/Program.cs
+++ WebApplicationV1/Program.cs
@@ -1,10 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
 builder.WebHost.UseUrls("http://[::]:5006");
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World");
-// Add services to the container.
-
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
