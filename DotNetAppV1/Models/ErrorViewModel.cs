59df3664f4e5c069950547acdbddec51f0a3e2ec
Marquito94 <marco.fernandezrolon@gmail.com>
2023-11-02 08:30:34 -0300

    Added fourth AWS files

---
+++ DotNetAppV1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetAppV1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
