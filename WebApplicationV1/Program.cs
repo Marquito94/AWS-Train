3abafa4b90ae8de9afd8424c7929e22a0e00a93f
Marquito94 <71236169+Marquito94@users.noreply.github.com>
2023-11-02 08:26:35 -0300

    Delete WebApplicationV1 directory

--- WebApplicationV1/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-
-builder.WebHost.UseUrls("http://[::]:5006");
-
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
